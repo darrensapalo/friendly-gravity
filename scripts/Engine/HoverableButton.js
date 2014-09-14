60577bc1299e442e865ef9316adf00fc65317b4e
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2014-09-14 19:27:41 +0800

    Hovering buttons work now.

--- scripts/Engine/HoverableButton.js
+++ scripts/Engine/HoverableButton.js
@@ -5,6 +5,10 @@ function HoverableButton(spriteName, hoverSpriteName, x, y, width, height, opaci
 
 	// To be implemented
 	this.hoverSprite.opacity = 0;
+
+	this.isRest = true;
+	this.tween = false;
+	this.tweenOut = false;
 }
 
 HoverableButton.prototype = Object.create(Button.prototype);
@@ -12,11 +16,34 @@ HoverableButton.prototype.constructor = HoverableButton;
 
 HoverableButton.prototype.update = function () {
 	var p = game.InputHandler.mouse;
-	this.hoverSprite.opacity = (this.contains(p)) ? 1 : 0;
+	if (this.contains(p))
+	{
+
+		if (this.isRest)
+		{
+			console.log("Creating tween to highlight");
+			this.tween = createjs.Tween.get(this.hoverSprite).to({ opacity:1 }, 350, createjs.Ease.quadOut).call(function(s) {s.tween = false;}, [this]);
+			this.isRest = false;
+		}
+	}else
+	{
+		if (this.isRest == false)
+		{
+			// If you haven't started tweening down, then begin tweening down
+			if (this.tweenOut == false && this.tween == false)
+			{
+				console.log("Creating tween to remove highlight");
+				this.tweenOut = createjs.Tween.get(this.hoverSprite).to({ opacity:0 }, 350, createjs.Ease.quadIn).call(function(s) {s.tweenOut = false; s.isRest = true;}, [this]);
+			}
+		}
+
+
+	}
+	
 }
 
 HoverableButton.prototype.draw = function(context){
-	this.hoverSprite.draw(context);
 	Button.prototype.draw.call(this, context);
+	this.hoverSprite.draw(context);
 	
 }
